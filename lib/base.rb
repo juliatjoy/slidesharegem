33aeedc0ac8b188e5b0a24af46d79df703d51330
Juliat Joy <juliatjoy@gmail.com>
2016-06-09 08:39:41 +0530

    Basic methods

--- lib/base.rb
+++ lib/base.rb
@@ -1,12 +1,13 @@
 require 'net/http'
 require 'uri'
+require 'faraday'
 
 module SlideShare
   class Base
     API_BASE_URL "http://www.slideshare.net/api/2"
     format :xml
 
-    attr_accessor :api_key, :shared_secret, :connection
+    attr_accessor :api_key, :shared_secret
 
     def initialize(api_key, shared_secret)
       @api_key = api_key
@@ -14,16 +15,17 @@ module SlideShare
       connection_setup
     end
 
-    def get(path, params)
-      uri = URI.parse(API_BASE_URL)
-      http = Net::HTTP.new(uri.host, uri.port)
-      response = http.request(Net::HTTP::Get.new(uri.request_uri))
+    def connection_setup
+      @connection = Faraday.new(url: API_BASE_URL) do |faraday|
+        faraday.request :json
+        faraday.adapter Faraday.default_adapter
+      end
     end
 
-    # def connection_setup
-    #   @connection = Faraday.new(url: API_BASE_URL) do |faraday|
-    #     faraday.request :json
-    #   end
-    # end
+    def valid_parameters
+      time = Time.now.to_i
+      hash = Digest::SHA1.hexdigest "#{@shared_secret}#{time_stamp}"
+      {api_key: @api_key, ts: time_stamp, hash: hash}
+    end
   end
 end
